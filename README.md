c24f2712945d55357566fe3ce1231cf62a00e5e3
Kuan-You Lin <linstudyhard1997@gmail.com>
2022-09-26 10:13:05 -0400

    Add 117

--- README.md
+++ README.md
@@ -106,3 +106,4 @@ It's 2022-23 hiring season. Let the grind begin. :heart_on_fire: :sweat_drops:
 | 387    | First Unique Character in a String                       | Easy       |                                            |
 | 253    | Meeting Rooms II                                         | Medium     |                                            |
 | 78     | Subsets                                                  | Medium     |                                            |
+| 117    | Populating Next Right Pointers in Each Node II           | Medium     |                                            |
---
+++ src/py/117. Populating Next Right Pointers in Each Node II/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        
+        queue = deque([root])
+        while queue:
+            new_queue = []
+            for i, node in enumerate(queue):
+                if i < len(queue)-1:
+                    node.next = queue[i+1]
+                if node.left:
+                    new_queue.append(node.left)
+                if node.right:
+                    new_queue.append(node.right)
+            queue = new_queue
+        return root
+
