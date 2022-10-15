9e51b37ed3c482212e80518ba61ad812529f07c0
Kuan-You Lin <linstudyhard1997@gmail.com>
2022-10-15 17:31:59 -0400

    Revise 424

--- src/py/424. Longest Repeating Character Replacement/424. Longest Repeating Character Replacement.py
+++ src/py/424. Longest Repeating Character Replacement/424. Longest Repeating Character Replacement.py
@@ -1,15 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        def find_max_cnt(cnt):
-            return max((c, i) for i, c in enumerate(cnt))[0]
-        
         cnt = [0] * 26
         j = 0
         out = 0
         for i, c in enumerate(s):
             c_ord = ord(c) - ord('A')
             cnt[c_ord] += 1
-            while j < i and i - j + 1 - find_max_cnt(cnt) > k:
+            while j < i and i - j + 1 - max(cnt) > k:
                 cnt[ord(s[j]) - ord('A')] -= 1
                 j += 1
             out = max(out, i - j + 1)
